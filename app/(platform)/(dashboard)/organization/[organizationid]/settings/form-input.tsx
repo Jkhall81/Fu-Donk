104850758e4e51bee1ae861b5db29466e20788b0
Jkhall81 <jason.kei.hall@gmail.com>
2024-02-06 05:11:25 -0700

    back to working on this, took a break from this for three or four days to work on an internship coding challenge project, now I am working on form components related to creating tables.

---
+++ app/(platform)/(dashboard)/organization/[organizationid]/settings/form-input.tsx
@@ -0,0 +1,11 @@
+export const FormInput = () => {
+  <div>
+    <input
+      id="title"
+      name="title"
+      required
+      placeholder="enter a board title"
+      className="border-black border p-1"
+    />
+  </div>;
+};
