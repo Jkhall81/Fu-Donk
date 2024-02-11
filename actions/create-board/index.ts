2ca4fe26c8e9f92bc9a7170c701095f8f06ede3d
Jkhall81 <jason.kei.hall@gmail.com>
2024-02-11 11:20:50 -0700

    ready for deployment.

--- actions/create-board/index.ts
+++ actions/create-board/index.ts
@@ -9,6 +9,7 @@ import { CreateBoard } from "./schema";
 import { createAuditLog } from "@/lib/create-audit-log";
 import { ACTION, ENTITY_TYPE } from "@prisma/client";
 import { incrementAvailableCount, hasAvailableCount } from "@/lib/org-limit";
+import { checkSubscription } from "@/lib/subsctiption";
 
 const handler = async (data: InputType): Promise<ReturnType> => {
   const { userId, orgId } = auth();
@@ -20,8 +21,9 @@ const handler = async (data: InputType): Promise<ReturnType> => {
   }
 
   const canCreate = await hasAvailableCount();
+  const isPro = await checkSubscription();
 
-  if (!canCreate) {
+  if (!canCreate && !isPro) {
     return {
       error: "You have reached your limit of free boards.  Please upgrade",
     };
@@ -66,7 +68,7 @@ const handler = async (data: InputType): Promise<ReturnType> => {
         imageLinkHTML,
       },
     });
-    if (canCreate) {
+    if (canCreate && !isPro) {
       await incrementAvailableCount();
     }
 
