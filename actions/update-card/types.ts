e41de8c6c2f71d6a2241a0f2f1e8b39ebc8203af
Jkhall81 <jason.kei.hall@gmail.com>
2024-02-11 11:53:21 -0700

    fixed one more issue that crashed the vercel build process, also set up a vercel postgres db. didnt even now they had that.  im pretty sure im using a free version.  need to double check and see if they have my billing information.  but i reconfigured the app to use their postgres db, i reset it with prisma, migrated the db again, and its connecting no problem.  now hopefully it should deploy with no issues.  then i just need to change some stripe payment related env variables.  and do the documentation.

--- actions/update-card/types.ts
+++ actions/update-card/types.ts
@@ -4,4 +4,4 @@ import { ActionState } from "@/lib/create-safe-action";
 import { UpdateCard } from "./schema";
 
 export type InputType = z.infer<typeof UpdateCard>;
-export type ReturnType = ActionState<InputType, Board>;
+export type ReturnType = ActionState<InputType, Card>;
